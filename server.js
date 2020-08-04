ce2f37f9f689826f63596598c087878ae798b31f
Combatd <combatdstar@gmail.com>
2020-08-04 10:30:51 -0700

    Host server on PORT'

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3030;
+
+app.get('/', (req, res) => {
+    res.send('You hit the HOME route!');
+});
+
+app.listen(PORT, (err) => {
+    if (err) { 
+        console.log(err); 
+    }
+    else { 
+    console.log(`Server listening on ${PORT}}`);
+    }
+});
