697de7286155289dbfea51e2fd50d044153540cc
Combatd <combatdstar@gmail.com>
2020-08-04 11:04:00 -0700

    Require express middleware

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3030;
 
-const PORT = 3030;
+
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
     res.send('You hit the HOME route!');
 });
+
 
 app.listen(PORT, (err) => {
     if (err) { 
         console.log(err); 
     }
     else { 
-    console.log(`Server listening on ${PORT}}`);
+    console.log(`Server listening on ${PORT}`);
     }
 });
